efad298d5ce6a5c83d02084a0528d44b2e434edd
cornBuddy <heorhi.sakovich@gmail.com>
2016-04-14 00:17:51 +0300

    basic routes added

--- app.js
+++ app.js
@@ -3,5 +3,12 @@
 const http = require('http');
 
 http.createServer((request, response) => {
-  // stuff here
+  const method = request.method;
+  const url = request.url;
+  if (method === 'GET' && url === '/')
+    null; // return main page
+  else if (method === 'POST' && url === '/points')
+    null; // get points from client
+  else if (method === 'GET' && url === '/result')
+    null; // show result
 }).listen(8080);
