a0d7e7814821f3419e82ac93240d1fe4b32aa897
cornBuddy <heorhi.sakovich@gmail.com>
2016-04-14 00:01:18 +0300

    test server removed

--- app.js
+++ app.js
@@ -3,23 +3,5 @@
 const http = require('http');
 
 http.createServer((request, response) => {
-  const headers = request.headers;
-  const method = request.method;
-  const url = request.url;
-  let body = [];
-  request
-    .on('error', (err) => console.error(err))
-    .on('data', (chunk) => body.push(chunk))
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
-      response.on('error', (err) => console.error(err));
-      response.writeHead(200, {'Content-Type': 'application/json'});
-      const responseBody = {
-        headers: headers,
-        method: method,
-        url: url,
-        body: body
-      };
-      response.end(JSON.stringify(responseBody));
-    });
+  // stuff here
 }).listen(8080);
