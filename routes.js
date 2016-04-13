eba95c0b4c3df309d44af640277304ce750b43ac
cornBuddy <heorhi.sakovich@gmail.com>
2016-04-14 00:46:40 +0300

    few changes

--- routes.js
+++ routes.js
@@ -1,4 +1,10 @@
-exports.renderMainPage = null;
+const readFile = require('fs').redFileSync;
+
+const index = readFile('.html/index.html');
+
+exports.renderMainPage = (req, res) => {
+
+};
 
 exports.getDataFromClient = null;
 
