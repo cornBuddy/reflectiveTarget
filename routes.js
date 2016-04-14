1ac7c353c18ac04766c0c2aea1e037db19a43e38
cornBuddy <heorhi.sakovich@gmail.com>
2016-04-14 11:57:13 +0300

    index page rendered

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
-const readFile = require('fs').redFileSync;
+const fs = require('fs');
 
-const index = readFile('.html/index.html');
+const index = fs.readFileSync('./html/index.html');
 
 exports.renderMainPage = (req, res) => {
-
+  res.statusCode = 200;
+  res.writeHeader(200, {'Content-Type': 'text/html'});
+  res.end(index);
 };
 
 exports.getDataFromClient = null;
@@ -11,3 +13,5 @@ exports.getDataFromClient = null;
 exports.sendResult = null;
 
 exports.show404Page = null;
+
+exports.show500Page = null;
