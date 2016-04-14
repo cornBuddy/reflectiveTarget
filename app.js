e08ad238a782427825ee254df33d84742c7f13bd
cornBuddy <heorhi.sakovich@gmail.com>
2016-04-14 12:05:59 +0300

    git repaired

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 'use strict';
 
 const http = require('http');
-const renderMainPage = require('.routes').renderMainPage;
-const getDataFromClient = require('.routes').getDataFromClient;
-const sendResult = require('.routes').sendResult;
-const show404Page = require('.routes').show404Page;
+const renderMainPage = require('./routes').renderMainPage;
+const getDataFromClient = require('./routes').getDataFromClient;
+const sendResult = require('./routes').sendResult;
+const show404Page = require('./routes').show404Page;
+const show500Page = require('./routes').show500Page;
 
 http.createServer((request, response) => {
   const method = request.method;
   const url = request.url;
+  request.on('error', (error) => show500Page(response, error));
   if (method === 'GET' && url === '/')
     renderMainPage(request, response);
   else if (method === 'POST' && url === '/points')
--- routes.js
+++ routes.js
@@ -1,7 +1,17 @@
-exports.renderMainPage = null;
+const fs = require('fs');
+
+const index = fs.readFileSync('./html/index.html');
+
+exports.renderMainPage = (req, res) => {
+  res.statusCode = 200;
+  res.writeHeader(200, {'Content-Type': 'text/html'});
+  res.end(index);
+};
 
 exports.getDataFromClient = null;
 
 exports.sendResult = null;
 
 exports.show404Page = null;
+
+exports.show500Page = null;
