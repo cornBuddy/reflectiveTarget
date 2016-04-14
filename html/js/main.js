98740a2af6ee532d01b79c3b8acc27c430f1ac9b
cornBuddy <heorhi.sakovich@gmail.com>
2016-04-15 01:24:54 +0300

    basic ajax post implementation added

--- html/js/main.js
+++ html/js/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('test');
+const targetImage = document.querySelector('img');
+targetImage.addEventListener('click', function(event) {
+  console.log('image clicked', event);
+  const mock = {
+    x: 1,
+    y: 1
+  };
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  };
+  const init = {
+    method: 'POST',
+    body: JSON.stringify(mock),
+    headers: headers
+  };
+  fetch('/points', init)
+    .then(() => console.log('ok'))
+    .catch((error) => console.log(error));
+});
