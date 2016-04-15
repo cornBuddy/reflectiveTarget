47549eedbbdbc10570bbb6c19efe139d4e4326f2
cornBuddy <heorhi.sakovich@gmail.com>
2016-04-15 19:50:41 +0300

    some changes in css, html and js

--- html/js/main.js
+++ html/js/main.js
@@ -1,21 +1,29 @@
 'use strict';
 
+let tapCounter = 0;
+let tapsCoordinates = [];
+
 const targetImage = document.querySelector('img');
 targetImage.addEventListener('click', function(event) {
-  const mock = {
-    x: 1,
-    y: 1
-  };
-  const headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  };
-  const init = {
-    method: 'POST',
-    body: JSON.stringify(mock),
-    headers: headers
-  };
-  fetch('/points', init)
-    .then(() => console.log('ok'))
-    .catch((error) => console.log(error));
+  tapCounter++;
+  if (tapCounter != 4) {
+    const tap = {
+      x: event.offsetX,
+      y: event.offsetY
+    };
+    tapsCoordinates.push(tap);
+  } else {
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+    const init = {
+      method: 'POST',
+      body: JSON.stringify(tapsCoordinates),
+      headers: headers
+    };
+    fetch('/points', init)
+      .then(() => console.log('ok'))
+      .catch((error) => console.log(error));
+  }
 });
