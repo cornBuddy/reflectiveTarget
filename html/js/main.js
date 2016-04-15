3d814e18426a2e5211bd60f7f1f8c7f073719860
cornBuddy <heorhi.sakovich@gmail.com>
2016-04-15 20:53:42 +0300

    points draws correctly

--- html/js/main.js
+++ html/js/main.js
@@ -2,6 +2,7 @@
 
 let tapCounter = 0;
 let tapsCoordinates = [];
+const targetImage = document.querySelector('img');
 
 let generateData = function(data, httpMethod='POST') {
   const headers = {
@@ -15,15 +16,37 @@ let generateData = function(data, httpMethod='POST') {
   };
 };
 
-const targetImage = document.querySelector('img');
+let parseObjects = function(response) {
+  return JSON.parse(response);
+};
+
+let displayResults = function(objects) {
+  for (let studentResult of objects) {
+    for (let point of studentResult) {
+      console.log(point);
+    }
+  }
+  return null;
+};
+
+let drawPoint = function(tap) {
+  let point = document.createElement('div');
+  let imageWrapper = document.getElementById('image-wrapper');
+  point.className = 'point';
+  point.style.marginTop = `${tap.y}px`;
+  point.style.marginLeft = `${tap.x}px`;
+  imageWrapper.insertBefore(point, targetImage);
+};
+
 targetImage.addEventListener('click', function(event) {
-  tapCounter++;
   if (tapCounter != 4) {
     const tap = {
       x: event.offsetX,
       y: event.offsetY
     };
     tapsCoordinates.push(tap);
+    drawPoint(tap);
+    tapCounter++;
   } else {
     const init = generateData(tapsCoordinates);
     fetch('/points', init)
